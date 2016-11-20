a1bb611a467a7721fb98f9aa69ff1ae73530b386
Nikhil Handa <nikhilhanda@live.com>
2016-11-21 02:41:47 +0530

    Navbar added

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -18,7 +18,33 @@
     return directive;
 
     /** @ngInject */
-    function NavbarController() {
+    function NavbarController($rootScope, $scope, $state) {
+      var vm = this;
+      vm.menuOpen=false;
+      if($state.current.name === 'add'){
+        vm.active = 0;
+      }else if($state.current.name === 'list'){
+        vm.active = 1;
+      }
+
+      $rootScope.$on('$stateChangeSuccess',
+      function(){
+        vm.menuOpen=false;
+      })
+
+      $scope.$on('$viewContentLoaded', function(){
+        if($state.current.name === 'add'){
+          vm.active = 0;
+        }else if($state.current.name === 'list'){
+          vm.active = 1;
+        }
+      });
+      vm.toggleMenu = function(){
+        if(vm.menuOpen)
+          vm.menuOpen=false;
+        else
+          vm.menuOpen=true;
+      }
     }
   }
 
